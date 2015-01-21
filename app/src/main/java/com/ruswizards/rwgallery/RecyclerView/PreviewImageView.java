ab5e50622d15deaa1a922aff98a8191d65785936
Vladimir Farafonov <vfarafonov@ruswizards.com>
2015-01-21 14:00:10 +0400

    Fixed scaling troubles with UIL library

--- app/src/main/java/com/ruswizards/rwgallery/RecyclerView/PreviewImageView.java
+++ app/src/main/java/com/ruswizards/rwgallery/RecyclerView/PreviewImageView.java
@@ -7,6 +7,7 @@
 package com.ruswizards.rwgallery.RecyclerView;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.ImageView;
@@ -52,5 +53,33 @@ public class PreviewImageView extends ImageView {
 	 */
 	public synchronized void setImageLoadAsyncTask(ImageLoadAsyncTask imageLoadAsyncTask) {
 		imageLoadAsyncTask_ = new WeakReference<ImageLoadAsyncTask>(imageLoadAsyncTask);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		try {
+			Drawable drawable = getDrawable();
+			if (drawable == null){
+				setMeasuredDimension(0, 0);
+			} else {
+				int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
+				int measuredHeight = MeasureSpec.getSize(heightMeasureSpec);
+				if (measuredHeight == 0 && measuredWidth == 0) { //Height and width set to wrap_content
+					setMeasuredDimension(measuredWidth, measuredHeight);
+				} else if (measuredHeight == 0) { //Height set to wrap_content
+					int width = measuredWidth;
+					int height = width *  drawable.getIntrinsicHeight() / drawable.getIntrinsicWidth();
+					setMeasuredDimension(width, height);
+				} else if (measuredWidth == 0){ //Width set to wrap_content
+					int height = measuredHeight;
+					int width = height * drawable.getIntrinsicWidth() / drawable.getIntrinsicHeight();
+					setMeasuredDimension(width, height);
+				} else { //Width and height are explicitly set (either to match_parent or to exact value)
+					setMeasuredDimension(measuredWidth, measuredHeight);
+				}
+			}
+		} catch (Exception e){
+			super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		}
 	}
 }
